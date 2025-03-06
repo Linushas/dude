c7be6da365722d3e8271ca64e11f8b256c58277a
Linus H <linus.hasselkvist@gmail.com>
2025-03-07 00:06:57 +0100

    Added #define's for keys and states

--- src/main.c
+++ src/main.c
@@ -1,33 +1,64 @@
 #include <ncurses.h>
+#include <stdbool.h>
 
-#define STATE_HELP 3
+#define STATE_STANDARD  0
+#define STATE_SPE_DIR   2
+#define STATE_HELP      3
 
-void draw(int state) 
+#define KEY_QUIT        'q'
+#define KEY_HELP_I      'h'
+#define KEY_ESC         27
+
+int initColor() 
 {
-    static int mx=0, my=0;
-    getmaxyx(stdscr, mx, my);
+    if (has_colors() == FALSE) 
+    {
+        printf("Your terminal does not support color\n");
+        return 0;
+    }
 
-    clear();
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_WHITE, COLOR_BLACK);
 
-    /// FRAME
+    return 1;
+}
+
+void printFrame(int mx, int my) 
+{
     attron(COLOR_PAIR(1));
     for(int i = 2; i < mx-2; i++) 
     {
         mvprintw(i, 1, "~");
     }
+}
 
-    /// STATES
-    if(!state) 
-    {
+void printStateHelp(int mx, int my)
+{
+    attron(COLOR_PAIR(2));
+    mvprintw(mx/2 -4, my/2 - 30, "DUDE - CLI Disk Usage Analyser");
+    mvprintw(mx/2 -2, my/2 - 30, "\tlink to docs: https://github.com/Linushas/dude");
+    attron(COLOR_PAIR(1));
+    mvprintw(mx/2 -3, my/2 - 30, "by Linus\t\t\t\t\t ");
+}
 
-    }
-    else if(state == STATE_HELP) 
-    {
-        attron(COLOR_PAIR(2));
-        mvprintw(mx/2 -4, my/2 - 30, "DUDE - CLI Disk Usage Analyser");
-        mvprintw(mx/2 -2, my/2 - 30, "\tlink to docs: https://github.com/Linushas/dude");
-        attron(COLOR_PAIR(1));
-        mvprintw(mx/2 -3, my/2 - 30, "by Linus\t\t\t\t\t ");
+void updateInterface(int state) 
+{
+    static int mx=0, my=0;
+    getmaxyx(stdscr, mx, my);
+
+    clear();
+    printFrame(mx, my);
+    switch(state) {
+        case STATE_STANDARD:
+
+            break;
+        case STATE_SPE_DIR:
+
+            break;
+        case STATE_HELP:
+            printStateHelp(mx, my);
+            break;
     }
 
     refresh();
@@ -35,39 +66,39 @@ void draw(int state)
 
 int main(int argc, char **argv) 
 {
-    int active = 1;
-    int state = 0;
-    initscr();
+    int active = true;
+    int state = STATE_STANDARD;
+    if(argc > 1) state = STATE_SPE_DIR;
+    /*if(!isValidPath(argv[0]))           --              TODO!
+    {
+        printf("%s is not a valid path\n", argv[0]);
+        return 1;
+    }*/
 
-    if (has_colors() == FALSE) 
+    initscr();
+    if(!initColor()) 
     {
         endwin();
-        printf("Your terminal does not support color\n");
         return 1;
-    }
+    } 
 
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_WHITE, COLOR_BLACK);
-
-    draw(state);
-
+    updateInterface(state);
     while(active) 
     {
         switch(getch()) 
         {
-            case 'q': 
-                active = 0;
+            case KEY_QUIT: 
+                active = false;
                 break;
-            case 'h':
+            case KEY_HELP_I:
                 state = STATE_HELP;
                 break;
-            case 27:
-                state = 0;
+            case KEY_ESC:
+                state = STATE_STANDARD;
                 break;
         }
 
-        draw(state);
+        updateInterface(state);
     }
 
     endwin();
